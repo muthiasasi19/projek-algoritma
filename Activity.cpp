a408fbca6cda0286f7ef4b6511f765218f5ca5b0
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-13 17:06:00 -0700

    git branch -M main

---
+++ Activity.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ // Begin 
+    // Numeric bil
+    int bil;
+    // Character status
+    string status;
+    // Display "Masukkan bilangan = ";
+    cout << "Masukkan bilangan = ";
+    // Accept bil
+    cin >> bil;
+    // if(bil == 0)
+    if (bil == 0)
+        //status = "nol"
+        status = "nol";
+    // else if (bil %2 == 0)
+    else if (bil %2 == 0)
+        // status = "genap";
+        status = "genap"; 
+    // else
+    else    
+        // status = "ganjil"
+        status = "ganjil";
+    // display "bilangannya = " + status
+    cout << "bilangannya = " << status;
+
+} // end
